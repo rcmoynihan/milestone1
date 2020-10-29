141b4bea35642caeeeae5336739cb890e8ee928b
Riley Moynihan <42723978+rcmoynihan@users.noreply.github.com>
2020-10-29 10:55:51 -0500

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,11 +3,18 @@ input.onButtonPressed(Button.A, function () {
         basic.showNumber(steps)
     }
 })
+function create_encoded_url (num: number) {
+    return "" + timeanddate.dateTime() + "|" + num
+}
 input.onButtonPressed(Button.AB, function () {
     steps = 0
 })
 input.onButtonPressed(Button.B, function () {
-	
+    bluetooth.advertiseUrl(
+    create_encoded_url(steps),
+    7,
+    false
+    )
 })
 input.onGesture(Gesture.Shake, function () {
     steps += 1
