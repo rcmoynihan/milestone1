5e1cd2dac43632bac60286795b32a74f25beda98
Riley Moynihan <42723978+rcmoynihan@users.noreply.github.com>
2020-10-29 11:04:49 -0500

    Update pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,5 +1,25 @@
-bluetooth.advertiseUrl(
-"",
-7,
-false
-)
+def on_button_pressed_a():
+    basic.show_number(steps)
+    basic.pause(1000)
+    basic.clear_screen()
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def create_encoded_url(num: number):
+    return "hey work dammit"
+
+def on_button_pressed_ab():
+    global steps
+    steps = 0
+input.on_button_pressed(Button.AB, on_button_pressed_ab)
+
+def on_button_pressed_b():
+    bluetooth.advertise_url(create_encoded_url(steps), 7, False)
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
+def on_gesture_shake():
+    global steps
+    steps += 1
+input.on_gesture(Gesture.SHAKE, on_gesture_shake)
+
+steps = 0
+steps = 0
--- main.ts
+++ main.ts
@@ -1,22 +1,21 @@
-input.onButtonPressed(Button.A, function () {
-    if (steps < 10) {
-        basic.showNumber(steps)
-    }
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    basic.showNumber(steps)
+    basic.pause(1000)
+    basic.clearScreen()
 })
-function create_encoded_url (num: number) {
-    return "" + timeanddate.dateTime() + "|" + num
+function create_encoded_url(num: number): string {
+    return "hey work dammit"
 }
-input.onButtonPressed(Button.AB, function () {
+
+input.onButtonPressed(Button.AB, function on_button_pressed_ab() {
+    
     steps = 0
 })
-input.onButtonPressed(Button.B, function () {
-    bluetooth.advertiseUrl(
-    create_encoded_url(steps),
-    7,
-    false
-    )
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
+    bluetooth.advertiseUrl(create_encoded_url(steps), 7, false)
 })
-input.onGesture(Gesture.Shake, function () {
+input.onGesture(Gesture.Shake, function on_gesture_shake() {
+    
     steps += 1
 })
 let steps = 0
