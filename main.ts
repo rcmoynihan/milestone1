42975612272a2d3ce20f301d3f1a9f7193be332e
Riley Moynihan <42723978+rcmoynihan@users.noreply.github.com>
2020-10-29 10:48:34 -0500

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -6,11 +6,11 @@ input.onButtonPressed(Button.A, function () {
 input.onButtonPressed(Button.AB, function () {
     steps = 0
 })
+input.onButtonPressed(Button.B, function () {
+	
+})
 input.onGesture(Gesture.Shake, function () {
     steps += 1
 })
 let steps = 0
 steps = 0
-basic.forever(function () {
-    basic.showNumber(steps)
-})
