cb7ccd3387a948e74d617466fa3c19a4e3f750e7
Sarah Teng <43072690+steng1106@users.noreply.github.com>
2020-11-02 17:56:14 -0600

    milestone 2

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 def on_button_pressed_a():
     basic.show_number(steps)
-    basic.pause(1000)
+    basic.pause(2000)
     basic.clear_screen()
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
 def create_encoded_url(num: number):
-    return "hey work dammit"
+    global url
+    url = "https://" + timeanddate.date_time()
+    return url
 
 def on_button_pressed_ab():
     global steps
@@ -13,13 +15,20 @@ def on_button_pressed_ab():
 input.on_button_pressed(Button.AB, on_button_pressed_ab)
 
 def on_button_pressed_b():
-    bluetooth.advertise_url(create_encoded_url(steps), 7, False)
+    create_encoded_url(steps)
+    bluetooth.advertise_url(url, 7, False)
+    basic.pause(2000)
+    bluetooth.stop_advertising()
 input.on_button_pressed(Button.B, on_button_pressed_b)
 
 def on_gesture_shake():
     global steps
     steps += 1
+    basic.show_number(steps)
+    basic.pause(100)
+    basic.clear_screen()
 input.on_gesture(Gesture.SHAKE, on_gesture_shake)
 
+url = ""
 steps = 0
 steps = 0
--- main.ts
+++ main.ts
@@ -1,22 +1,31 @@
-input.onButtonPressed(Button.A, function on_button_pressed_a() {
+input.onButtonPressed(Button.A, function () {
     basic.showNumber(steps)
-    basic.pause(1000)
+    basic.pause(2000)
     basic.clearScreen()
 })
-function create_encoded_url(num: number): string {
-    return "hey work dammit"
+function create_encoded_url (num: number) {
+    url = "https://" + steps
+    return url
 }
-
-input.onButtonPressed(Button.AB, function on_button_pressed_ab() {
-    
+input.onButtonPressed(Button.AB, function () {
     steps = 0
 })
-input.onButtonPressed(Button.B, function on_button_pressed_b() {
-    bluetooth.advertiseUrl(create_encoded_url(steps), 7, false)
+input.onButtonPressed(Button.B, function () {
+    create_encoded_url(steps)
+    bluetooth.advertiseUrl(
+    url,
+    7,
+    false
+    )
+    basic.pause(2000)
+    bluetooth.stopAdvertising()
 })
-input.onGesture(Gesture.Shake, function on_gesture_shake() {
-    
+input.onGesture(Gesture.Shake, function () {
     steps += 1
+    basic.showNumber(steps)
+    basic.pause(100)
+    basic.clearScreen()
 })
+let url = ""
 let steps = 0
 steps = 0
